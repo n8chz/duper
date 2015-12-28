2af5d5bcfecf8418d9cbf347ac677040df7d2367
Lorraine Lee <n8chz@yahoo.ca>
2015-12-28 02:00:40 -0500

    created helper for foreign key autofill pairs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,7 @@ module ApplicationHelper
   group =
    text_field_tag nil, nil, :data => {value: value, source: source}
   group << "\n"
-  group <<
-   (button_tag "+", :data => {class: "#{fkey}"}, :type => "button", :title => "create new #{fkey}")
+  group << (button_tag "+", :data => {class: "#{fkey}"}, :type => "button", :title => "create new #{fkey}")
   group << "\n"
   group << (text_field_tag "#{model}[#{index}][#{fkey}_id]", nil, :id => value, :type => "hidden")
   group
